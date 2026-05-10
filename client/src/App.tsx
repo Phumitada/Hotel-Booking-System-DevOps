0cd980eaccdf02f0ffb11176092c9d53acf5460e
Phumitada <phumitada@gmail.com>
2026-05-10 21:00:05 +0700

    Remove ExplorePage import from App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ import HomePage from "@/pages/home";
 import LoginPage from "@/pages/login";
 import RegisterPage from "@/pages/register";
 import HotelsPage from "@/pages/hotels";
-import ExplorePage from "@/pages/explore";
 import RoomsPage from "@/pages/rooms";
 import BookingPage from "@/pages/Booking";
 import BookingsPage from "@/pages/bookings";
